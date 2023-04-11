3d0eb2a13d06340f3e723033c74caec945015b53
ricoatsproutasia <arisandy.rico@sproutasia.com>
2023-04-11 15:37:35 +0700

    remove unused pack

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ app.use(cors());
 const PORT = process.env.PORT || 3000;
 
 // Route to upload file
+app.get('/', async (req, res) => {
+    res.json({
+        message: "it works"
+    });
+});
+
+// Route to upload file
 app.post('/upload', async (req, res) => {
     const form = formidable({ multiples: true });
     const file = await new Promise((resolve) =>
